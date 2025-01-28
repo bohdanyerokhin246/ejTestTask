334ac128147fff139da7275bbb19c5cbfa87d95b
bohdan <erohinbogdan246@gmail.com>
2025-01-28 23:28:04 +0200

    Changed name and type of Buyer variable

--- config/order.go
+++ config/order.go
@@ -1,8 +1,11 @@
 package config
 
+import "time"
+
 type Order struct {
-	ID       int       `json:"ID,omitempty"`
-	Buyer    string    `json:"buyer,omitempty"`
-	Price    float64   `json:"price,omitempty"`
-	Products []Product `json:"products,omitempty"`
+	ID        int       `json:"ID,omitempty"`
+	BuyerID   int       `json:"buyerID,omitempty"`
+	Price     float64   `json:"price,omitempty"`
+	Products  []Product `json:"products,omitempty"`
+	CreatedAt time.Time `json:"createdAt,omitempty"`
 }
