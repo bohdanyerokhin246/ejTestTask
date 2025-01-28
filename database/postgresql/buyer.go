737419ea2c47e3f1509e9ec6f965d61f75c8e9b1
bohdan <erohinbogdan246@gmail.com>
2025-01-28 23:25:28 +0200

    Created CRUD functions for buyer entity

--- database/postgresql/buyer.go
+++ database/postgresql/buyer.go
@@ -1,1 +1,32 @@
 package postgresql
+
+import (
+	"database/sql"
+	"ejTestTask/config"
+)
+
+func CreateBuyer(db *sql.DB, buyer config.Buyer) (int, error) {
+	var id int
+	query := `INSERT INTO buyers (name, phone) VALUES ($1, $2) RETURNING id`
+	err := db.QueryRow(query, buyer.Name, buyer.Phone).Scan(&id)
+	return id, err
+}
+
+func GetBuyerByID(db *sql.DB, id int) (config.Buyer, error) {
+	var buyer config.Buyer
+	query := `SELECT id, name, phone FROM buyers WHERE id = $1`
+	err := db.QueryRow(query, id).Scan(&buyer.ID, &buyer.Name, &buyer.Phone)
+	return buyer, err
+}
+
+func UpdateBuyer(db *sql.DB, buyer config.Buyer) error {
+	query := `UPDATE buyers SET name = $1, phone = $2 WHERE id = $3`
+	_, err := db.Exec(query, buyer.Name, buyer.Phone, buyer.ID)
+	return err
+}
+
+func DeleteBuyer(db *sql.DB, id int) error {
+	query := `DELETE FROM buyers WHERE id = $1`
+	_, err := db.Exec(query, id)
+	return err
+}
