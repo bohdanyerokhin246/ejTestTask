c357d483e1888c78c76451365d40b2c35d91e841
bohdan <erohinbogdan246@gmail.com>
2025-01-30 02:24:09 +0200

    Added API and DB handlers for Buyer

--- database/postgresql/buyer.go
+++ database/postgresql/buyer.go
@@ -1,32 +1,59 @@
 package postgresql
 
 import (
-	"database/sql"
 	"ejTestTask/config"
+	"ejTestTask/database"
 )
 
-func CreateBuyer(db *sql.DB, buyer config.Buyer) (int, error) {
+func CreateBuyer(buyer config.Buyer) (int, error) {
 	var id int
 	query := `INSERT INTO buyers (name, phone) VALUES ($1, $2) RETURNING id`
-	err := db.QueryRow(query, buyer.Name, buyer.Phone).Scan(&id)
+	err := database.PsqlDB.QueryRow(query, buyer.Name, buyer.Phone).Scan(&id)
 	return id, err
 }
 
-func GetBuyerByID(db *sql.DB, id int) (config.Buyer, error) {
+func GetBuyerByID(id int) (config.Buyer, error) {
 	var buyer config.Buyer
 	query := `SELECT id, name, phone FROM buyers WHERE id = $1`
-	err := db.QueryRow(query, id).Scan(&buyer.ID, &buyer.Name, &buyer.Phone)
+	err := database.PsqlDB.QueryRow(query, id).Scan(&buyer.ID, &buyer.Name, &buyer.Phone)
 	return buyer, err
 }
 
-func UpdateBuyer(db *sql.DB, buyer config.Buyer) error {
+func GetBuyers() ([]config.Buyer, error) {
+	var buyers []config.Buyer
+
+	query := `SELECT * FROM buyers`
+	rows, err := database.PsqlDB.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var buyer config.Buyer
+
+		err = rows.Scan(&buyer.ID, &buyer.Name, &buyer.Phone)
+		if err != nil {
+			return nil, err
+		}
+		buyers = append(buyers, buyer)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return buyers, nil
+}
+
+func UpdateBuyer(buyer config.Buyer) error {
 	query := `UPDATE buyers SET name = $1, phone = $2 WHERE id = $3`
-	_, err := db.Exec(query, buyer.Name, buyer.Phone, buyer.ID)
+	_, err := database.PsqlDB.Exec(query, buyer.Name, buyer.Phone, buyer.ID)
 	return err
 }
 
-func DeleteBuyer(db *sql.DB, id int) error {
+func DeleteBuyer(id int) error {
 	query := `DELETE FROM buyers WHERE id = $1`
-	_, err := db.Exec(query, id)
+	_, err := database.PsqlDB.Exec(query, id)
 	return err
 }
--- transport/json/buyer.go
+++ transport/json/buyer.go
@@ -1,1 +1,133 @@
 package json
+
+import (
+	"database/sql"
+	"ejTestTask/config"
+	"ejTestTask/database/postgresql"
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+func CreateBuyerHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			http.Error(w, `{"error":"Invalid request method"}`, http.StatusMethodNotAllowed)
+			return
+		}
+
+		var buyer config.Buyer
+		if err := json.NewDecoder(r.Body).Decode(&buyer); err != nil {
+			http.Error(w, fmt.Sprintf(`{"error":"Invalid JSON payload. Error: %v"}`, err), http.StatusBadRequest)
+			return
+		}
+
+		id, err := postgresql.CreateBuyer(buyer)
+		if err != nil {
+			http.Error(w, fmt.Sprintf(`{"error":"Failed to create buyer. Error: %v"}`, err), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusCreated)
+		json.NewEncoder(w).Encode(map[string]int{"id": id})
+	}
+}
+
+func GetBuyerByIDHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			http.Error(w, `{"error":"Invalid request method"}`, http.StatusMethodNotAllowed)
+			return
+		}
+
+		var buyer config.Buyer
+
+		if err := json.NewDecoder(r.Body).Decode(&buyer); err != nil || buyer.ID <= 0 {
+			http.Error(w, fmt.Sprintf(`{"error":"Invalid JSON payload. Error: %v"}`, err), http.StatusBadRequest)
+			return
+		}
+
+		buyer, err := postgresql.GetBuyerByID(buyer.ID)
+		if err != nil {
+			if err == sql.ErrNoRows {
+				http.Error(w, `{"error":"Buyer not found"}`, http.StatusNotFound)
+			} else {
+				http.Error(w, fmt.Sprintf(`{"error":"Failed to fetch buyer. Error: %v"}`, err), http.StatusInternalServerError)
+			}
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(buyer)
+	}
+}
+
+func GetBuyersHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			http.Error(w, `{"error":"Invalid request method"}`, http.StatusMethodNotAllowed)
+			return
+		}
+
+		buyers, err := postgresql.GetBuyers()
+		if err != nil {
+			if err == sql.ErrNoRows {
+				http.Error(w, `{"error":"Order not found"}`, http.StatusNotFound)
+			} else {
+				http.Error(w, fmt.Sprintf(`{"error":"Failed to fetch buyers. Error: %v"}`, err), http.StatusInternalServerError)
+			}
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(buyers)
+	}
+}
+
+func UpdateBuyerHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPut {
+			http.Error(w, `{"error":"Invalid request method"}`, http.StatusMethodNotAllowed)
+			return
+		}
+
+		var buyer config.Buyer
+
+		if err := json.NewDecoder(r.Body).Decode(&buyer); err != nil || buyer.ID <= 0 {
+			http.Error(w, fmt.Sprintf(`{"error":"Invalid JSON payload. Error: %v"}`, err), http.StatusBadRequest)
+			return
+		}
+
+		if err := postgresql.UpdateBuyer(buyer); err != nil {
+			http.Error(w, fmt.Sprintf(`{"error":"Failed to update buyer. Error: %v"}`, err), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(map[string]string{"message": fmt.Sprintf("Buyer %d updated", buyer.ID)})
+	}
+}
+
+func DeleteBuyerHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodDelete {
+			http.Error(w, `{"error":"Invalid request method"}`, http.StatusMethodNotAllowed)
+			return
+		}
+
+		var buyer config.Buyer
+		if err := json.NewDecoder(r.Body).Decode(&buyer); err != nil || buyer.ID <= 0 {
+			http.Error(w, fmt.Sprintf(`{"error":"Invalid JSON payload. Error: %v"}`, err), http.StatusBadRequest)
+			return
+		}
+
+		if err := postgresql.DeleteBuyer(buyer.ID); err != nil {
+			http.Error(w, fmt.Sprintf(`{"error":"Failed to delete buyer. Error: %v"}`, err), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(map[string]string{"message": fmt.Sprintf("Buyer %d deleted", buyer.ID)})
+	}
+}
