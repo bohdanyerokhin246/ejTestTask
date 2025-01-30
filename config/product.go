6770b0a8e3d6e2b16b770d2f3505d0c35a0d74a9
bohdan <erohinbogdan246@gmail.com>
2025-01-30 02:23:41 +0200

    Added API and DB handlers for Product

--- config/product.go
+++ config/product.go
@@ -5,5 +5,6 @@ type Product struct {
 	Name        string  `json:"name,omitempty"`
 	Description string  `json:"description,omitempty"`
 	Price       float64 `json:"price,omitempty"`
+	Quantity    int     `json:"quantity,omitempty"`
 	SellerID    int     `json:"sellerID,omitempty"`
 }
--- database/postgresql/product.go
+++ database/postgresql/product.go
@@ -7,21 +7,48 @@ import (
 
 func CreateProduct(product config.Product) (int, error) {
 	var id int
-	query := `INSERT INTO products (name, description, price, seller_id) VALUES ($1, $2, $3) RETURNING id`
-	err := database.PsqlDB.QueryRow(query, product.Name, product.Description, product.Price, product.SellerID).Scan(&id)
+	query := `INSERT INTO products (name, description, price, quantity, seller_id) VALUES ($1, $2, $3, $4, $5) RETURNING id`
+	err := database.PsqlDB.QueryRow(query, product.Name, product.Description, product.Price, product.Quantity, product.SellerID).Scan(&id)
 	return id, err
 }
 
 func GetProductByID(id int) (config.Product, error) {
 	var product config.Product
-	query := `SELECT id, name, description, price, seller_id FROM products WHERE id = $1`
-	err := database.PsqlDB.QueryRow(query, id).Scan(&product.ID, &product.Name, &product.Description, &product.Price, &product.SellerID)
+	query := `SELECT * FROM products WHERE id = $1`
+	err := database.PsqlDB.QueryRow(query, id).Scan(&product.ID, &product.Name, &product.Description, &product.Price, &product.Quantity, &product.SellerID)
 	return product, err
 }
 
+func GetProducts() ([]config.Product, error) {
+	var products []config.Product
+
+	query := `SELECT * FROM products`
+	rows, err := database.PsqlDB.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var product config.Product
+
+		err = rows.Scan(&product.ID, &product.Name, &product.Description, &product.Price, &product.Quantity, &product.SellerID)
+		if err != nil {
+			return nil, err
+		}
+		products = append(products, product)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return products, nil
+}
+
 func UpdateProduct(product config.Product) error {
-	query := `UPDATE products SET name = $1, description = $2, price = $3 WHERE id = $4`
-	_, err := database.PsqlDB.Exec(query, product.Name, product.Description, product.Price, product.ID)
+	query := `UPDATE products SET name = $1, description = $2, price = $3, quantity = $4, seller_id = $5 WHERE id = $6`
+	_, err := database.PsqlDB.Exec(query, product.Name, product.Description, product.Price, product.Quantity, product.SellerID, product.ID)
 	return err
 }
 
