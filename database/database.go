b985608ca0be82c5ba0501cf5b66a43c9b800060
bohdan <erohinbogdan246@gmail.com>
2025-01-31 01:57:45 +0200

    Changed .env variables

--- database/database.go
+++ database/database.go
@@ -13,12 +13,12 @@ func Connect() {
 	var err error
 	connStr := fmt.Sprintf(
 		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
-		os.Getenv("hostDB"),
-		os.Getenv("portDB"),
-		os.Getenv("userDB"),
-		os.Getenv("passwordDB"),
-		os.Getenv("ejNameDB"),
-		os.Getenv("sslModeDB"))
+		os.Getenv("DB_HOST_APP"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("SSL_MODE"))
 
 	PsqlDB, err = sql.Open("postgres", connStr)
 	if err != nil {
