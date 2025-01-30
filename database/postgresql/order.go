e84cab5e1fabdb7a656c1a94e75bfc489ec07bbe
bohdan <erohinbogdan246@gmail.com>
2025-01-30 02:23:20 +0200

    Added API and DB handlers for Order

--- database/postgresql/order.go
+++ database/postgresql/order.go
@@ -3,25 +3,113 @@ package postgresql
 import (
 	"ejTestTask/config"
 	"ejTestTask/database"
+	"encoding/json"
+	"fmt"
 )
 
+// CreateOrder is creating order in next way:
+// 1. Get config.Order from request (in request body we don`t have order.Price and order.Products[].Price)
+// 2. Order.Price = for range order.Products[] {product.Quantity * product.Price}
+// 3. Reduce product.Quantity for each product in order
 func CreateOrder(order config.Order) (int, error) {
 	var id int
+	var sum float64
+	var err error
+	var productsJSON []byte
+
+	for i := range order.Products {
+		product := &order.Products[i]
+
+		productFromDB, err := GetProductByID(product.ID)
+		if err != nil {
+			fmt.Println("Product not found")
+			continue
+		}
+
+		sum += float64(product.Quantity) * productFromDB.Price
+		product.Price = productFromDB.Price
+
+		productFromDB.Quantity -= product.Quantity
+		err = UpdateProduct(productFromDB)
+		if err != nil {
+			fmt.Println("Error with update")
+		}
+	}
+
+	productsJSON, err = json.Marshal(order.Products)
+	if err != nil {
+		return 0, err
+	}
+
 	query := `INSERT INTO orders (buyer_id, price, products) VALUES ($1, $2, $3) RETURNING id`
-	err := database.PsqlDB.QueryRow(query, order.BuyerID, order.Price, order.Products).Scan(&id)
+	err = database.PsqlDB.QueryRow(query, order.BuyerID, sum, productsJSON).Scan(&id)
+	if err != nil {
+		fmt.Println(err)
+	}
+
 	return id, err
 }
 
 func GetOrderByID(id int) (config.Order, error) {
 	var order config.Order
+	var productsJSON []byte
 	query := `SELECT id, buyer_id, price, products, created_at FROM orders WHERE id = $1`
-	err := database.PsqlDB.QueryRow(query, id).Scan(&order.ID, &order.BuyerID, &order.Price, &order.Products, &order.CreatedAt)
+	err := database.PsqlDB.QueryRow(query, id).Scan(&order.ID, &order.BuyerID, &order.Price, &productsJSON, &order.CreatedAt)
+
+	err = json.Unmarshal(productsJSON, &order.Products)
+	if err != nil {
+		return order, err
+	}
+
 	return order, err
 }
 
+func GetOrders() ([]config.Order, error) {
+	var orders []config.Order
+
+	query := `SELECT id, buyer_id, price, products, created_at FROM orders`
+	rows, err := database.PsqlDB.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var order config.Order
+		var productsJSON []byte
+
+		err := rows.Scan(&order.ID, &order.BuyerID, &order.Price, &productsJSON, &order.CreatedAt)
+		if err != nil {
+			return nil, err
+		}
+
+		err = json.Unmarshal(productsJSON, &order.Products)
+		if err != nil {
+			return nil, fmt.Errorf("error unmarshaling products JSON: %w", err)
+		}
+
+		orders = append(orders, order)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return orders, nil
+}
+
 func UpdateOrder(order config.Order) error {
+
+	var err error
+	var productsJSON []byte
+	productsJSON, err = json.Marshal(order.Products)
+
+	if err != nil {
+		return err
+	}
+
 	query := `UPDATE orders SET buyer_id = $1, products = $2 WHERE id = $3`
-	_, err := database.PsqlDB.Exec(query, order.BuyerID, order.Products, order.ID)
+	_, err = database.PsqlDB.Exec(query, order.BuyerID, productsJSON, order.ID)
 	return err
 }
 
--- transport/json/order.go
+++ transport/json/order.go
@@ -1,1 +1,133 @@
 package json
+
+import (
+	"database/sql"
+	"ejTestTask/config"
+	"ejTestTask/database/postgresql"
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+func CreateOrderHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			http.Error(w, `{"error":"Invalid request method"}`, http.StatusMethodNotAllowed)
+			return
+		}
+
+		var order config.Order
+		if err := json.NewDecoder(r.Body).Decode(&order); err != nil {
+			http.Error(w, `{"error":"Invalid JSON payload"}`, http.StatusBadRequest)
+			return
+		}
+
+		id, err := postgresql.CreateOrder(order)
+		if err != nil {
+			http.Error(w, fmt.Sprintf(`{"error":  "Failed to create order. Error: %v"}`, err), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusCreated)
+		json.NewEncoder(w).Encode(map[string]int{"id": id})
+	}
+}
+
+func GetOrderByIDHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			http.Error(w, `{"error":"Invalid request method"}`, http.StatusMethodNotAllowed)
+			return
+		}
+
+		var order config.Order
+
+		if err := json.NewDecoder(r.Body).Decode(&order); err != nil || order.ID <= 0 {
+			http.Error(w, `{"error":"Invalid JSON payload"}`, http.StatusBadRequest)
+			return
+		}
+
+		order, err := postgresql.GetOrderByID(order.ID)
+		if err != nil {
+			if err == sql.ErrNoRows {
+				http.Error(w, `{"error":"Order not found"}`, http.StatusNotFound)
+			} else {
+				http.Error(w, fmt.Sprintf(`{"error":"Failed to fetch order. Error: %v"}`, err), http.StatusInternalServerError)
+			}
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(order)
+	}
+}
+
+func GetOrdersHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			http.Error(w, `{"error":"Invalid request method"}`, http.StatusMethodNotAllowed)
+			return
+		}
+
+		orders, err := postgresql.GetOrders()
+		if err != nil {
+			if err == sql.ErrNoRows {
+				http.Error(w, `{"error":"Order not found"}`, http.StatusNotFound)
+			} else {
+				http.Error(w, fmt.Sprintf(`{"error":"Failed to fetch order. Error: %v"}`, err), http.StatusInternalServerError)
+			}
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(orders)
+	}
+}
+
+func UpdateOrderHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPut {
+			http.Error(w, `{"error":"Invalid request method"}`, http.StatusMethodNotAllowed)
+			return
+		}
+
+		var order config.Order
+
+		if err := json.NewDecoder(r.Body).Decode(&order); err != nil || order.ID <= 0 {
+			http.Error(w, `{"error":"Invalid JSON payload"}`, http.StatusBadRequest)
+			return
+		}
+
+		if err := postgresql.UpdateOrder(order); err != nil {
+			http.Error(w, `{"error":"Failed to update order"}`, http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(map[string]string{"message": "Order updated"})
+	}
+}
+
+func DeleteOrderHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodDelete {
+			http.Error(w, `{"error":"Invalid request method"}`, http.StatusMethodNotAllowed)
+			return
+		}
+
+		var order config.Order
+		if err := json.NewDecoder(r.Body).Decode(&order); err != nil || order.ID <= 0 {
+			http.Error(w, `{"error":"Invalid JSON payload"}`, http.StatusBadRequest)
+			return
+		}
+
+		if err := postgresql.DeleteOrder(order.ID); err != nil {
+			http.Error(w, `{"error":"Failed to delete order"}`, http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(map[string]string{"message": fmt.Sprintf("Order %d deleted", order.ID)})
+	}
+}
